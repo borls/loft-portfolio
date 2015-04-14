5eb3ef3f9a718df8fdf45cadb2fe1eb3175e8900
borls <by@monosnap.com>
2015-04-14 23:45:20 +0300

    first page

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-> МОЙ ПРОЕКТ
+> Site-Portfolio (Yuzhakov Boris)
 
 Как его установить?
-===================
+-------------------
 
-1. git clone https://github.com/borls/loft-portfolio.git portfolio
+1. git clone https://github.com/borls/loft-portfolio.git
 2. npm i
 3. bower i
 4. gulp
---
+++ app/img/social/style-social.sql
@@ -0,0 +1,53 @@
+.sprite {
+    background-image: url(/img/social/sprite-social.png);
+    background-repeat: no-repeat;
+    display: block;
+}
+
+.sprite-icon-fb {
+    width: 47px;
+    height: 47px;
+    background-position: -5px -5px;
+}
+
+.sprite-icon-fb {
+    width: 47px;
+    height: 47px;
+    background-position: -62px -5px;
+}
+
+.sprite-icon-git {
+    width: 47px;
+    height: 47px;
+    background-position: -119px -5px;
+}
+
+.sprite-icon-git {
+    width: 47px;
+    height: 47px;
+    background-position: -176px -5px;
+}
+
+.sprite-icon-tw {
+    width: 47px;
+    height: 47px;
+    background-position: -233px -5px;
+}
+
+.sprite-icon-tw {
+    width: 47px;
+    height: 47px;
+    background-position: -290px -5px;
+}
+
+.sprite-icon-vk {
+    width: 47px;
+    height: 47px;
+    background-position: -347px -5px;
+}
+
+.sprite-icon-vk {
+    width: 47px;
+    height: 47px;
+    background-position: -404px -5px;
+}
